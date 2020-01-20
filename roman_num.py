69e114fd5f1687371e7aee559b349403a0471a86
ripleycmd <natalia.marek@outlook.com>
2020-01-20 01:47:19 +0000

    modified tests and added class + methods

--- roman_num.py
+++ roman_num.py
@@ -1,8 +1,16 @@
-def roman_numeral():
-    if not 0 < input < 4000:
-        raise ValueError           #and print("Please enter number between 1 and 3999")
-    if type(input) != type(6):
-        raise ValueError                  #Index Error?
+class Roman:
+
+    def roman_numeral():
+        if not 0 < input < 4000:
+            print ('Please enter a number between 1 and 3999')
+        if type(input) != type(6):
+            print(f'{input} is not a number between 1-3999. Please enter a number between 1 and 3999')
+
+
+
+
+        #Symbol 	I 	V 	X 	L 	C  	D 	     M
+        #Value 	   1 	5 	10 	   50 	100 	500 	1,000 
 
 
 
--- roman_num_test.py
+++ roman_num_test.py
@@ -3,9 +3,11 @@ from roman_num import roman_numeral
 
 class TestRomanNumerals(unittest.TestCase):
     def test_If_Input_Is_Int(self):
-        self.assertEqual(roman_numeral(0),0)
-    def test_IfInputWithin0and4000(self):
+        self.assertEqual("This is not a number", ValueError)
+    def test_If_Input_Within_0_and_4000(self):
         self.assertEqual(4001, IndexError)
+    def test_integer_to_roman_numeral(self):
+        self.assertEqual(10, "X")
     
 if __name__ == '__main__':
     unittest.main()
