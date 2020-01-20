356a64edf202a3b71eb3eb59e84c9ba88fe39269
ripleycmd <natalia.marek@outlook.com>
2020-01-20 01:28:44 +0000

    Tdd - run and failed 2 initial tests

--- roman_num_test.py
+++ roman_num_test.py
@@ -1,12 +1,11 @@
 import unittest
 from roman_num import roman_numeral
 
-class RomanNumeralsTest(unittest.TestCase):
-    def TestIfInputIsInt(self):
-        self.assertEqual("NotANumber", ValueError)
-
-    def TestIfInputWithin0and4000(self):
-        self.assertEqual(4001, ValueError)
-
+class TestRomanNumerals(unittest.TestCase):
+    def test_If_Input_Is_Int(self):
+        self.assertEqual(roman_numeral(0),0)
+    def test_IfInputWithin0and4000(self):
+        self.assertEqual(4001, IndexError)
+    
 if __name__ == '__main__':
     unittest.main()
