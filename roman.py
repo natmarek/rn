0ece161d0251891618ee07a1b9ed478a6e7a1584
ripleycmd <natalia.marek@outlook.com>
2020-01-22 16:53:33 +0000

    Changes in unit test functions

--- roman.py
+++ roman.py
@@ -4,12 +4,14 @@ class Roman:
         pass
 
     def int_to_roman(input):
-        if type(input) != type(1):
+        if type(input) != type(6):
             raise TypeError 
         if not 0 < input < 4000:
             raise ValueError 
+        
         ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
         nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
+        
         result = ""
         for i in range(len(ints)):
             count = int(input / ints[i])
